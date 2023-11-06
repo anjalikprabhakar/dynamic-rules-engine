608093da1063e6a71dc969be6fbe2e32d2e299b2
Anjalikprabhakar <anjali.k@edstem.com>
2023-11-06 16:23:22 +0530

    initial commit

---
+++ src/main/java/com/dynamicrulesengine/dynamicrulesengine/DynamicRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.dynamicrulesengine.dynamicrulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicRulesEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamicrulesengine/dynamicrulesengine/DynamicRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamicrulesengine.dynamicrulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicRulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
