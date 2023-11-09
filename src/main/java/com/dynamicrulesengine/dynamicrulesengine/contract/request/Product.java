b0f3275ef34c44bd16b387efe3e57eed04160eab
MEGHANA K <meghana.k@edstem.com>
2023-11-09 10:45:06 +0530

    InventoryReplenishmentRule api added

---
+++ src/main/java/com/dynamicrulesengine/dynamicrulesengine/contract/request/Product.java
@@ -0,0 +1,9 @@
+package com.dynamicrulesengine.dynamicrulesengine.contract.request;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String name;
+    private int stockLevel;
+}
---
+++ src/main/java/com/dynamicrulesengine/dynamicrulesengine/contract/request/ProductRequest.java
@@ -0,0 +1,9 @@
+package com.dynamicrulesengine.dynamicrulesengine.contract.request;
+
+import lombok.Data;
+
+@Data
+public class ProductRequest {
+    private String productId;
+    private Product product;
+}
